99230266fa486f44d1fba1da67b1e0e4eeee083d
Elena <ukr.7905432@gmail.com>
2022-07-09 15:00:12 -0600

    load complementation

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let x = +prompt("Введите первое число");
+let y = +prompt("Введите второе число");
+
+/*let z = +(x + y);
+console.log(`Результат:  ${x} + ${y} = ${z}`);*/
+
+/*let s = +(x - y);
+console.log(`Результат:  ${x} - ${y} = ${s}`);*/
+
+/*let s = +(x * y);
+console.log(`Результат:  ${x} * ${y} = ${s}`);*/
+
+let s = +(x / y);
+console.log(`Результат:  ${x} / ${y} = ${s}`);
